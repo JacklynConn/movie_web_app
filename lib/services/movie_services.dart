5d71d3bfadef09e1cceacf5b83230ea2be62d202
Mak Mach <471842@ntti.edu.kh>
2024-09-03 00:38:01 +0700

    #17 making http request parsing movies data

---
+++ lib/services/movie_services.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:flutter_web_app/models/movie_model.dart';
+import 'package:http/http.dart' as http;
+
+class MovieServices {
+  final baseUrl = 'https://api.themoviedb.org/3/movie/';
+  final headers = {
+    'Authorization':
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzEyNGJhMDE2YmExZTcyM2FkZDgyMzZhNjE0NTY0YSIsIm5iZiI6MTcyNTI5NjkwMy4yODgxMjYsInN1YiI6IjY1NTYzOGJkZDA1MWQ5MDEzYTE3MjFlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fGbrQzI0yh0nYsWGIoBeNGQW3L-HF_Qp7RbQmSs3z-c',
+    'accept': 'application/json',
+  };
+
+  Future<List<Movie>> fetchTopRatedMovies() async {
+    final response = await http.get(
+        Uri.parse('${baseUrl}top_rated?language=en-US&page=1'),
+        headers: headers);
+
+    if (response.statusCode == 200) {
+      return ((jsonDecode(response.body)['results']) as List)
+          .map((data) => Movie.fromJson(data))
+          .toList();
+    } else {
+      throw Exception('Failed to fetch top rated movies');
+    }
+  }
+}
