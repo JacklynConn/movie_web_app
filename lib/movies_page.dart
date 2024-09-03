9a290d870be4333978f0779a9e488c9b73e030fe
Mak Mach <471842@ntti.edu.kh>
2024-09-03 17:13:13 +0700

    #26 movies filter page

--- lib/movies_page.dart
+++ lib/movies_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_app/services/movie_services.dart';
 import 'package:flutter_web_app/widgets/drawer.dart';
 import 'package:flutter_web_app/widgets/navbar.dart';
+
+import 'models/movie_model.dart';
+import 'movies_widget/movies_filter.dart';
 
 class MoviesPage extends StatefulWidget {
   const MoviesPage({super.key});
@@ -10,13 +14,42 @@ class MoviesPage extends StatefulWidget {
 }
 
 class _MoviesPageState extends State<MoviesPage> {
+  List<Movie> topRatedMovies = [];
+  List<Movie> nowPlayingMovies = [];
+  List<Movie> popularMovies = [];
+  List<Movie> upcomingMovies = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    MovieServices movieServices = MovieServices();
+    topRatedMovies = await movieServices.fetchTopRatedMovies();
+    nowPlayingMovies = await movieServices.fetchNowPlayingMovies();
+    popularMovies = await movieServices.fetchPopularMovies();
+    upcomingMovies = await movieServices.fetchUpComingMovies();
+    setState(() {
+      isLoading = false;
+    });
+    // debugPrint(topRatedMovies[0].title);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: CustomDrawer(),
       appBar: CustomNavbar(),
       body: Center(
-        child: Text('Movies Page'),
+        child: MoviesFilter(
+          topRatedMovies: topRatedMovies,
+          nowPlayingMovies: nowPlayingMovies,
+          popularMovies: popularMovies,
+          upcomingMovies: upcomingMovies,
+        ),
       ),
     );
   }
---
+++ lib/movies_widget/movies_filter.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+import '../models/movie_model.dart';
+
+class MoviesFilter extends StatefulWidget {
+  final List<Movie> topRatedMovies;
+  final List<Movie> nowPlayingMovies;
+  final List<Movie> popularMovies;
+  final List<Movie> upcomingMovies;
+  int selectedFilterIndex;
+
+  MoviesFilter({
+    super.key,
+    required this.topRatedMovies,
+    required this.nowPlayingMovies,
+    required this.popularMovies,
+    required this.upcomingMovies,
+    this.selectedFilterIndex = 0,
+  });
+
+  @override
+  State<MoviesFilter> createState() => _MoviesFilterState();
+}
+
+class _MoviesFilterState extends State<MoviesFilter> {
+  List<Movie> movies = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {
+                widget.selectedFilterIndex = 0;
+                movies = widget.topRatedMovies;
+                setState(() {});
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.shade800,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  'Popular',
+                  style: TextStyle(
+                    color: widget.selectedFilterIndex == 0
+                        ? const Color(0xFFE2B616)
+                        : Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 5),
+            GestureDetector(
+              onTap: () {
+                widget.selectedFilterIndex = 1;
+                movies = widget.nowPlayingMovies;
+                setState(() {});
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.shade800,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  'Now Playing',
+                  style: TextStyle(
+                    color: widget.selectedFilterIndex == 1
+                        ? const Color(0xFFE2B616)
+                        : Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 5),
+            GestureDetector(
+              onTap: () {
+                widget.selectedFilterIndex = 2;
+                movies = widget.popularMovies;
+                setState(() {});
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.shade800,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  'Top Rated',
+                  style: TextStyle(
+                    color: widget.selectedFilterIndex == 2
+                        ? const Color(0xFFE2B616)
+                        : Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 5),
+            GestureDetector(
+              onTap: () {
+                widget.selectedFilterIndex = 3;
+                movies = widget.upcomingMovies;
+                setState(() {});
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.shade800,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  'Upcoming',
+                  style: TextStyle(
+                    color: widget.selectedFilterIndex == 3
+                        ? const Color(0xFFE2B616)
+                        : Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
